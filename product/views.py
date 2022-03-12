9c0ef46ee22fe74e101e98a8020dce22c4e52458
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2022-03-12 20:27:53 -0300

    Create Product model view set with redis cache

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 
-# Create your views here.
+from product.serializers import ProductModelSerializer
+from product.models import Product
+
+
+class ProductModelViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductModelSerializer
+
+    @method_decorator(cache_page(settings.CACHE_TTL))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)

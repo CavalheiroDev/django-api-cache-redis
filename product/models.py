6a5bbe4a60ec88122007ecf0589b622de6f99e8b
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2022-03-12 20:15:15 -0300

    Feat: create model Product and migration

--- product/models.py
+++ product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True)
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    price = models.PositiveIntegerField(help_text="Valor do produto em centavos")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
